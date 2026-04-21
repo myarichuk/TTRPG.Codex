fb7bc7c3aad10a812a9beaac80579f4a988d6355
Your Name <michaely@bindecy.com>
2026-04-22 01:28:07 +0300

    Update IAbilityDefinition.cs: Add SystemId to IAbilityDefinition.

---
+++ src/Codex.Plugin.Abstractions/IAbilityDefinition.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Codex.Plugin.Abstractions;
+
+public interface IAbilityDefinition
+{
+    string Id { get; }
+    string SystemId { get; }
+    string Name { get; }
+    string? Description { get; }
+    string? IconPath { get; }
+    TriggerType Trigger { get; }
+    string? Inherits { get; }
+    Dictionary<string, int>? Costs { get; }
+    List<IAbilityEffect>? Effects { get; }
+    Dictionary<string, object>? Metadata { get; }
+}
