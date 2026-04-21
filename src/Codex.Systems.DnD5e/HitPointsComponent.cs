1c8c99ed50181b34fac2adfd2ee36b1a447256ea
Your Name <michaely@bindecy.com>
2026-04-22 02:43:08 +0300

    Update HitPointsComponent.cs: Mark HitPointsComponent as Obsolete in favor of ResourcePoolComponent.

--- src/Codex.Systems.DnD5e/HitPointsComponent.cs
+++ src/Codex.Systems.DnD5e/HitPointsComponent.cs
@@ -1,7 +1,11 @@
+using System;
+using Codex.Core.Components;
+
 namespace Codex.Systems.DnD5e;
 
+[Obsolete("Use ResourcePoolComponent with 'HP' and 'HP_Max' keys instead.")]
 public struct HitPointsComponent
 {
-    public int Current;
-    public int Maximum;
+    public int Current { get; set; }
+    public int Maximum { get; set; }
 }
