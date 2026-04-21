29b925a65f0811b0804bc97d9f045257cdc336d5
Your Name <michaely@bindecy.com>
2026-04-22 01:28:36 +0300

    Update IAbilityRegistry.cs: Add Register with priority to IAbilityRegistry.

---
+++ src/Codex.Plugin.Abstractions/IAbilityRegistry.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Codex.Plugin.Abstractions;
+
+public interface IAbilityRegistry
+{
+    void Register(IAbilityDefinition ability);
+    void Register(IAbilityDefinition ability, int priority);
+    IAbilityDefinition? GetAbility(string fullId);
+    IEnumerable<IAbilityDefinition> GetAbilitiesBySystem(string systemId);
+}
